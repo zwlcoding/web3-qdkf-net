4a9a9d57531d40800d0e9d4507f0ede24c493d07
Waylon <porzhang@gmail.com>
2024-07-09 16:12:26 +0800

    feat: 修改跳转

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,10 +1,11 @@
 import { useTranslation } from 'react-i18next';
+import {Link} from '@tanstack/react-router'
 const Menu = () => {
     const { t } = useTranslation();
     return (
         <>
-            <li><a href="/home">{t('menu_home')}</a></li>
-            <li><a href="/about">{t('menu_about')}</a></li>
+            <li><Link to="/">{t('menu_home')}</Link></li>
+            <li><Link to="/about">{t('menu_about')}</Link></li>
         </>
     )
 }
